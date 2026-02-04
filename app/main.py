b9cc4e2eba3c7ee9075e37a46d438aedc7f0d5ef
prabakaran202 <prabakaran20020430@gmail.com>
2026-02-04 22:44:29 +0530

    Add login route and request/response schemas

--- app/main.py
+++ app/main.py
@@ -1,7 +1,9 @@
 from fastapi import FastAPI
 from routes.health import router as health_router
+from routes.login import router as login_router
 app = FastAPI()
 app.include_router(health_router)
+app.include_router(login_router)
 
 @app.get("/")
 def read_root():
