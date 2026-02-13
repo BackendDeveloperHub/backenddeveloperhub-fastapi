9c5da00aecf47f36b6da123c506145c33af053db
marvintawww <marvintawww@gmail.com>
2026-02-13 19:14:39 +0300

    feat: Add Product API endpoint with schema and router

--- app/main.py
+++ app/main.py
@@ -1,9 +1,11 @@
 from fastapi import FastAPI
 from routes.health import router as health_router
 from routes.ping import router as ping_router
+from routes.products import router as product_router
 app = FastAPI()
 app.include_router(health_router)
 app.include_router(ping_router)
+app.include_router(product_router)
 @app.get("/")
 def read_root():
     return {"msg": "Welcome to the Backend Developer Hub FastAPI!"}
